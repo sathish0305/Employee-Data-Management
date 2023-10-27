c0b19bc59a62e46ada26ebe23061be7dedcc7abe
Sathish M <sathishms0305@gmail.com>
2023-10-27 13:36:58 +0530

    created employee models

--- employeeApp/models.py
+++ employeeApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeData(models.Model):
+    employee_id = models.CharField(max_length=10)
+    employee_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone = models.IntegerField()
+    address = models.CharField(max_length=200)
+    hire_date =  models.DateField()
+    position = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.employee_name
