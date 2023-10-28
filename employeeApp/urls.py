a134bd64379017cff99d71fbe5a06a0d36644b72
Sathish M <sathishms0305@gmail.com>
2023-10-28 10:42:56 +0530

    employee updateview

--- employeeApp/urls.py
+++ employeeApp/urls.py
@@ -6,4 +6,5 @@ app_name = 'empapp'
 urlpatterns = [
     path('',views.EmployeeList.as_view(),name='list'),
     path('create/',views.EmployeeCreateView.as_view(),name='create'),
+    path('update/<int:pk>/',views.EmployeeUpdateView.as_view(),name='update'),
 ]
--- employeeApp/views.py
+++ employeeApp/views.py
@@ -1,5 +1,7 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView
 from employeeApp.models import EmployeeData
 
 # Create your views here.
@@ -9,7 +11,12 @@ def index(request):
 class EmployeeList(ListView):
     context_object_name = 'Employeelist'
     model = EmployeeData
+    ordering=['employee_id']
 
 class EmployeeCreateView(CreateView):
+    model = EmployeeData
+    fields = '__all__'
+
+class EmployeeUpdateView(UpdateView):
     model = EmployeeData
     fields = '__all__'
