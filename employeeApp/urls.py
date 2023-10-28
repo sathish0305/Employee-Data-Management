d0efb7562fd61cfe63b7a6e3eacd96a3c8a5cc8a
Sathish M <sathishms0305@gmail.com>
2023-10-28 21:46:42 +0530

    Employee Delete

--- employeeApp/urls.py
+++ employeeApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',views.EmployeeList.as_view(),name='list'),
     path('create/',views.EmployeeCreateView.as_view(),name='create'),
     path('update/<int:pk>/',views.EmployeeUpdateView.as_view(),name='update'),
-]
+    path('delete/<int:pk>/',views.EmployeeDeleteView.as_view(),name='delete'),
+    ]
--- employeeApp/views.py
+++ employeeApp/views.py
@@ -1,8 +1,9 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView,CreateView,UpdateView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from employeeApp.models import EmployeeData
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -20,3 +21,8 @@ class EmployeeCreateView(CreateView):
 class EmployeeUpdateView(UpdateView):
     model = EmployeeData
     fields = '__all__'
+
+class EmployeeDeleteView(DeleteView):
+    context_object_name = 'EmployeeDelete'
+    model = EmployeeData
+    success_url = reverse_lazy('empapp:list')
